1ff23b6d576468d2a8e79df9d650b8110d6cc468
karthikeyan-ks <karthikeyanks3673@gmail.com>
2026-03-06 01:44:31 +0530

    updated for testing

--- index.ts
+++ index.ts
@@ -5,6 +5,11 @@ import authRouter from "./routes/auth.routes";
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World! Your Express server is running.");
+});
+
 app.use(authenticateToken);
 app.use("/auth", authRouter);
 
